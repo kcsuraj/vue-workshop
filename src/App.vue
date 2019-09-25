1ece3bb855622bbfc467529a5a177aab41a930c3
fm-pc-lt-13 <suraj.kc@outlook.com>
2019-09-25 11:37:39 +0545

    add photo comment challenge

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <photo-comment></photo-comment>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PhotoComment from "./components/PhotoComment.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    PhotoComment
   }
 };
 </script>
---
+++ src/components/PhotoComment.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="comment">
+        <div class="comment__img">
+          <img src="../assets/ts.jpg" alt="Profile picture" />
+        </div>
+        <div class="comment__content">
+          <ul class="comment-list">
+            <li>
+              <span></span>
+            </li>
+          </ul>
+          <div class="no-comments">
+            <p>No comments added yet</p>
+          </div>
+        </div>
+        <div class="comment-box">
+          <textarea class="form-control" rows="3" placeholder="Write a comment"></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PhotoComment"
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  margin: 0 auto;
+  max-width: 768px;
+}
+
+.card {
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+}
+
+.comment {
+  &__img {
+    > img {
+      width: 100%;
+    }
+  }
+  &__content {
+    padding: 18px;
+
+    .comment-list {
+      > li {
+        &:not(:last-child) {
+          margin-bottom: 12px;
+        }
+        > span {
+          background-color: #f2f3f5;
+          border-radius: 68px;
+          padding: 8px 12px;
+          font-size: 14px;
+          display: inline-block;
+        }
+      }
+    }
+  }
+  .comment-box {
+    textarea {
+      border: 1px solid #f2f2f2;
+      width: 100%;
+      padding: 12px;
+    }
+  }
+}
+</style>
