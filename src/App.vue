dd86e283a86c1046d2176cd3e0e66240d01aa8f5
fm-pc-lt-13 <suraj.kc@outlook.com>
2019-09-25 10:39:00 +0545

    add calculator and anicipated movie challenge

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CalculatorMovie />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CalculatorMovie from "./components/CalculatorMovie.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    CalculatorMovie
   }
 };
 </script>
---
+++ src/components/CalculatorMovie.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container">
+    <div class="card calculator">
+      <div class="header">
+        <h4 class="title">Display sum between two numbers</h4>
+      </div>
+      <div class="content">
+        <div class="form-group">
+          <input type="number" class="form-control" placeholder="First Number" />
+        </div>
+        <div class="form-group">
+          <input type="number" class="form-control" placeholder="Second Number" />
+        </div>
+
+        <div class="form-group">
+          <p>The sum is</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="card movie">
+      <div class="header">
+        <h4 class="title">Which is your most anticipated movie ?</h4>
+      </div>
+
+      <div class="content">
+        <div class="form-group">
+          <select class="form-control">
+            <option>Doctor Strange in the Multiverse of Madness</option>
+            <option>Thor: Love and Thunder</option>
+            <Option>Joker</Option>
+          </select>
+        </div>
+        <div class="form-group">
+          <p>My most anticipated movie is:</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.container {
+  margin: 0 auto;
+  max-width: 768px;
+}
+
+.title {
+  font-size: 18px;
+  font-weight: 600;
+  margin-bottom: 18px;
+}
+
+.card {
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+  padding: 24px;
+}
+
+.calculator {
+  margin-bottom: 24px;
+}
+
+.form-group {
+  &:not(:last-child) {
+    margin-bottom: 18px;
+  }
+}
+
+.form-control {
+  display: block;
+  width: 100%;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+</style>
