a44e36b8af9c8bfdb8a94061a7b5f4b0cca69ccb
fm-pc-lt-13 <suraj.kc@outlook.com>
2019-09-28 11:57:40 +0545

    add calculator movie app

--- src/components/CalculatorMovie.vue
+++ src/components/CalculatorMovie.vue
@@ -6,14 +6,24 @@
       </div>
       <div class="content">
         <div class="form-group">
-          <input type="number" class="form-control" placeholder="First Number" />
+          <input
+            v-model.number="num1"
+            type="number"
+            class="form-control"
+            placeholder="First Number"
+          />
         </div>
         <div class="form-group">
-          <input type="number" class="form-control" placeholder="Second Number" />
+          <input
+            v-model.number="num2"
+            type="number"
+            class="form-control"
+            placeholder="Second Number"
+          />
         </div>
 
         <div class="form-group">
-          <p>The sum is</p>
+          <p>The sum is {{num1 + num2}}</p>
         </div>
       </div>
     </div>
@@ -25,14 +35,14 @@
 
       <div class="content">
         <div class="form-group">
-          <select class="form-control">
+          <select v-model="movie" class="form-control">
             <option>Doctor Strange in the Multiverse of Madness</option>
             <option>Thor: Love and Thunder</option>
             <Option>Joker</Option>
           </select>
         </div>
-        <div class="form-group">
-          <p>My most anticipated movie is:</p>
+        <div v-if="movie" class="form-group">
+          <p>My most anticipated movie is: {{movie}}</p>
         </div>
       </div>
     </div>
@@ -40,7 +50,16 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "CalculatorMovie",
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      movie: ""
+    };
+  }
+};
 </script>
 
 <style scoped lang="scss">
